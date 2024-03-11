9d11ea9580b6e971e879a2a1b8a20093e73bc8fa
Pedro_Mendes <130801080+PedroMendesMacedo@users.noreply.github.com>
2024-03-11 08:20:01 -0300

    Update Q2.C

--- ATIVIDADE_DE_SALA(LISTA-7)/Q2.C
+++ ATIVIDADE_DE_SALA(LISTA-7)/Q2.C
@@ -4,23 +4,23 @@ int main()
 {
     int i = 50;
     float r = 4.65;
-    char c = "Pedro";
+    char c = 'P';
     
     int *i2 = &i;
     float *r2 = &r;
     char *c2 = &c;
     
     printf("Este é o valor de inteiro:%d \n ", i);
-    *i2 = 40;
-    printf("Este é o novo valor de inteiro:%d \n ", *i2);
-    
-    printf("Este é o valor de real:%f \n ", r);    
+    printf("Este é o valor de real:%f \n ", r);  
+    printf("Este é o valor de char:%c \n ", c);    
+
+    *i2 = 40;    
     *r2 = 3.87;
+    *c2 = 'M';
+
+    printf("Este é o novo valor de inteiro:%d \n ", *i2);
     printf("Este é o novo valor de real:%f \n ", *r2);
-    
-    printf("Este é o valor de inteiro:%s \n ", c);    
-    *c2 = "Maria";
-    printf("Este é o novo valor de inteiro:%s \n ", *c2);
+    printf("Este é o novo valor de char:%c \n ", *c2);
     
     return 0;
 }
